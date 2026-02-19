23af9a4443bf72b5054aa6452c7dce4eb18ef224
haze-helloworld <jjttanvi@gmail.com>
2026-02-19 14:44:25 +0530

    changes for render

--- server.js
+++ server.js
@@ -1,47 +1,27 @@
-import http from "http";
-import { WebSocketServer } from "ws";
-
-const PORT = process.env.PORT || 10000;
-
-// MUST have an HTTP server
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("WebSocket server running");
-});
-
-// attach WS to HTTP upgrade
-const wss = new WebSocketServer({ noServer: true });
-
-server.on("upgrade", (req, socket, head) => {
-  wss.handleUpgrade(req, socket, head, (ws) => {
-    wss.emit("connection", ws, req);
-  });
-});
-
-const clients = new Set();
-
 wss.on("connection", (ws) => {
-  console.log("connected");
   clients.add(ws);
 
-  ws.send(JSON.stringify({ type: "system", text: "connected to server" }));
+  // tell everyone someone joined
+  broadcast({ type: "system", text: "user joined" });
 
   ws.on("message", (data) => {
-    let msg;
-    try {
-      msg = JSON.parse(data.toString());
-    } catch {
-      msg = { type: "text", text: data.toString() };
-    }
+    const text = data.toString();
 
-    const out = JSON.stringify(msg);
-
-    for (const c of clients) {
-      if (c.readyState === 1) c.send(out);
-    }
+    // send exactly what the user typed
+    broadcast({
+      type: "message",
+      text: text
+    });
   });
 
-  ws.on("close", () => clients.delete(ws));
+  ws.on("close", () => {
+    clients.delete(ws);
+    broadcast({ type: "system", text: "user left" });
+  });
 });
 
-server.listen(PORT, () => console.log("running on", PORT));
+function broadcast(obj) {
+  const msg = JSON.stringify(obj);
+  for (const c of clients)
+    if (c.readyState === 1) c.send(msg);
+}
